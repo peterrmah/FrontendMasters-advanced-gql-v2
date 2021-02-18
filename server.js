71a3a8ec6507ef4882dc8466dced374cc58d90f3
Peter Mah <peter.r.mah@gmail.com>
2021-02-18 16:27:12 -0700

    Demo server

---
+++ server.js
@@ -0,0 +1,70 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+    createdAt: Int!
+  }
+
+  type Settings {
+    user: User!
+    theme: String!
+  }
+
+  input NewSettingsInput {
+    user: ID!
+    theme: String!
+  }
+
+  type Query {
+    me: User!
+    settings(user: ID!): Settings!
+  }
+
+  type Mutation {
+    settings(input: NewSettingsInput!): Settings!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me() {
+      return {
+        id: 1,
+        username: "coder12",
+        createdAt: 23048230,
+      };
+    },
+    settings(_, { user }) {
+      return {
+        user,
+        theme: "light",
+      };
+    },
+  },
+
+  Mutation: {
+    settings(_, { input }) {
+      return input;
+    },
+  },
+
+  Settings: {
+    user(settings) {
+      return {
+        id: 1,
+        username: "coder12",
+        createdAt: 23048230,
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`🚀 Server running at ${url}`));
